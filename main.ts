2421435cf1ad4f81983bf55e3a66b532b27e95a6
therainisme <therainisme@qq.com>
2024-02-14 20:39:01 +0800

    update main.ts

--- main.ts
+++ main.ts
@@ -24,42 +24,42 @@ interface ImageInfo {
   title: string;
   url: string;
   copyright: string;
-  enddate: string;
+  startdate: string;
 };
 
 async function writeImageBuffer(imageInfo: ImageInfo) {
-  const { url, enddate } = imageInfo;
+  const { url, startdate } = imageInfo;
   const buffer = await fetchImageBuffer(baseUrl + url);
 
   // It can work without checking for duplication.
-  fs.writeFileSync(path.join(__dirname, "archive", `${enddate}.jpg`), new DataView(buffer));
+  fs.writeFileSync(path.join(__dirname, "archive", `${startdate}.jpg`), new DataView(buffer));
 };
 
 async function writeImageInfo(imageInfo: ImageInfo) {
   const jsonPath = path.join(__dirname, "archive", `information.json`);
-  const { title, copyright, enddate } = imageInfo;
+  const { title, copyright, startdate } = imageInfo;
   let jsonStr = fs.readFileSync(jsonPath, "utf8");
   let imageInfos: ImageInfo[] = JSON.parse(jsonStr);
 
   // If the imageInfo is already in the csv, do nothing.
-  if (!imageInfos.find((x) => x.enddate === enddate)) {
-    imageInfos.push({ enddate, title, copyright } as any);
+  if (!imageInfos.find((x) => x.startdate === startdate)) {
+    imageInfos.push({ startdate, title, copyright } as any);
     jsonStr = JSON.stringify(imageInfos, null, 2);
     fs.writeFileSync(jsonPath, jsonStr);
 
-    console.info(`Image info of ${enddate} has been written.`);
+    console.info(`Image info of ${startdate} has been written.`);
   }
 
   // Update README.md
   const templatePath = path.join(__dirname, "template.md");
   const readmePath = path.join(__dirname, "README.md");
   let readmeStr = fs.readFileSync(templatePath, "utf8");
-  readmeStr += `\n\n![${title}](./archive/${enddate}.jpg)\n\n`;
+  readmeStr += `\n\n![${title}](./archive/${startdate}.jpg)\n\n`;
   readmeStr += `### List of Included Wallpapers\n\n`;
   readmeStr += `|date|title|copyright|\n|---|---|---|\n`;
-  imageInfos.sort((a, b) => ~~b.enddate - ~~a.enddate);
+  imageInfos.sort((a, b) => ~~b.startdate - ~~a.startdate);
   imageInfos.forEach((x) => {
-    readmeStr += `|${x.enddate}|${x.title}|${x.copyright}|\n`;
+    readmeStr += `|${x.startdate}|${x.title}|${x.copyright}|\n`;
   });
   fs.writeFileSync(readmePath, readmeStr);
 }
